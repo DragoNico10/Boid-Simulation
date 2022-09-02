51125396f8a5ffce18516d0a15ebcf8c1125064e
DragoNico10 <arce.nico10@gmail.com>
2022-09-02 18:12:00 -0500

    WHY IS MAEH SPRITES POSITIONS TURNING INTO NaN

--- boid.js
+++ boid.js
@@ -1,6 +1,86 @@
 class Boid{
-    constructor(){
-        super()
-        
+    constructor(id){
+        this.distance;this.target
+        this.id=id
+        this.sprite=createSprite(random(0, width), random(0, height), 48, 48)
+        this.color=[Math.floor(random(0, 255)), Math.floor(random(0, 255)), Math.floor(random(0, 255))]
+        this.sprite.draw=()=>{
+            push()
+            fill(this.color[0], this.color[1], this.color[2])
+            noStroke()
+            triangle(-8, -5, -8, 5, 8, 0)
+            pop()
+        }
+        boids.push(this)
+        this.vX=random(-5, 5)
+        this.vY=random(-5, 5)
+        this.distX
+        this.distY
+        boidX.push(this.sprite.x)
+        boidY.push(this.sprite.y)
+        boidVX.push(this.vX)
+        boidVY.push(this.vY)
+    }
+    run(){
+        this.move(this.sprite.x, this.sprite.y)
+        this.calculate()
+    }
+    move(lx, ly){
+        this.sprite.x=width/2-this.vX
+        this.sprite.y=height/2-this.vY
+        this.sprite.pointTo(0, 0)
+        this.distance=dist(this.sprite.x, this.sprite.y, 0, 0)
+        this.target=(this.vX/this.distance)*TargetSpeed
+        this.vX+=Resolve*(this.target-this.vX)
+        this.target=(this.vY/this.distance)*TargetSpeed
+        this.vY+=Resolve*(this.target-this.vY)
+        this.sprite.x=lx+this.vX
+        this.sprite.y=ly+this.vY
+        if(this.sprite.y<0){
+            this.sprite.y+=height
+        }
+        if(this.sprite.y>height){
+            this.sprite.y-=height
+        }
+        if(this.sprite.x<0){
+            this.sprite.x+=width
+        }
+        if(this.sprite.x>width){
+            this.sprite.x-=width
+        }
+        boidX[this.id]=this.sprite.x
+        boidY[this.id]=this.sprite.y
+        boidVX[this.id]=this.vX
+        boidVY[this.id]=this.vY
+    }
+    calculate(){
+        let negSep=0-Separation
+        let otherId=0
+        let boidCount=0
+        let sumX=0
+        let SVX=0
+        let sumY=0
+        let SVY=0
+        for(let i = 0;i<boids.length;i++){
+            if(this.id!=otherId){
+                this.distX=boidX[otherId]-this.sprite.x
+                this.distY=boidY[otherId]-this.sprite.y
+                this.distance=Math.sqrt((this.distX*this.distX)+(this.distY*this.distY))
+                if(this.distance<Range){
+                    boidCount++
+                    sumX+=this.distX
+                    sumY+=this.distY
+                    SVX+=boidVX[otherId]-this.vX
+                    SVY+=boidVY[otherId]-this.vY
+                    this.vX+=negSep*(this.distX/this.distance)
+                    this.vY+=negSep*(this.distY/this.distance)
+                }
+            }
+            otherId++
+        }
+        this.vX+=Cohesion*(sumX/boidCount)
+        this.vY+=Cohesion*(sumY/boidCount)
+        this.vX+=Alignment*(SVX/boidCount)
+        this.vY+=Alignment*(SVX/boidCount)
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-
-
+var boids=[]
+var boidX=[]
+var boidY=[]
+var boidVX=[]
+var boidVY=[]
+var gui
+var TargetSpeed=2.85, Resolve=0.2, Range=75, Separation=0.2, Cohesion=0.03, Alignment=0.03
+let params={
+    TargetSpeedMin:0.50, TargetSpeedMax:5.00, TargetSpeedStep:0.01,TargetSpeed:TargetSpeed,
+    ResolveMin:0.01, ResolveMax:1.00, ResolveStep:0.01,Resolve:Resolve,
+    RangeMin:20, RangeMax:150,RangeStep:1,Range:Range,
+    SeparationMin:0.00, SeparationMax:0.50, SeparationStep:0.01,Separation:Separation,
+    CohesionMin:0.00, CohesionMax:0.50, CohesionStep:0.01,Cohesion:Cohesion,
+    AlignmentMin:0.00, AlignmentMax:0.10, AlignmentStep:0.01, Alignment:Alignment
+}
 function setup(){
-    canvas=createCanvas(240*2, 180*2)
-    translate(240, 180)
+    canvas=createCanvas(windowWidth, windowHeight)
+    let l=width*height
+    createBoids(10)
+    gui=createGui('Boid Control')
+    gui.addObject(params)
+    frameRate(60)
+}
+function draw(){
+    background(0)
+    correctVars()
+
+    for(let boid of boids){
+        boid.run()
+    }
+    drawSprites()
+}
+let createBoids =(num)=>{
+    for(let i = 0;i<num;i++){
+        new Boid(i)
+    }
+}
+var windowResized=()=>{
+    resizeCanvas(windowWidth, windowHeight)
+}
+let correctVars=()=>{
+    TargetSpeed=params.TargetSpeed
+    Resolve=params.Resolve
+    Range=params.Range
+    Separation=params.Separation
+    Cohesion=params.Cohesion
+    Alignment=params.Alignment
 }
